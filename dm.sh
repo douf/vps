aecf1f594457982a1bf2a8974883a4aa28b15327
douf <douf@users.noreply.github.com>
2017-10-17 18:56:43 -0700

    Update dm.sh

--- dm.sh
+++ dm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sh ./dmUpload.sh -u doudoufuk@yahoo.com.hk -p jl6639@@ -k 6bf483507ef62528ab3c -s f685b33ae21696c5ba1eabd40f93ed922a65ff3f -c TV -t "test" -l Chinese 123.mp4 test
+sh ./dmUpload.sh -u doudoufuk@yahoo.com.hk -p jl6639@@ -k 6bf483507ef62528ab3c -s f685b33ae21696c5ba1eabd40f93ed922a65ff3f -c TV 123.mp4
