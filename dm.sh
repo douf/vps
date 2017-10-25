ea476f9e16853fff7e0205537e7dc92b18b3e5f7
douf <douf@users.noreply.github.com>
2017-10-25 13:54:24 -0700

    Update dm.sh

--- dm.sh
+++ dm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sh ./dmUpload.sh -u moneyman10548@yahoo.com.hk -p aa29440099 -k 1fbb1d1ca972096cf95f -s 44105eb0968f60be5c403978fbd431f57ff07576 -c TV 123.mp4
+sh ./dmUpload.sh -u moneyman10548@yahoo.com.hk -p ********** -k 1fbb1d1ca972096cf95f -s 44105eb0968f60be5c403978fbd431f57ff07576 -c TV 123.mp4
