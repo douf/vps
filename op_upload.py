f6bb32754d51fdf6e9958417fea66854bc474740
douf <douf@users.noreply.github.com>
2018-12-07 21:21:58 -0800

    Update op_upload.py

--- op_upload.py
+++ op_upload.py
@@ -6,5 +6,5 @@ key = os.environ.get('OPENLOAD_KEY')
 
 ol = OpenLoad('login', 'key')
 
-uploaded_file_info = ol.upload_file('/usr/movie/92985.jpg')
+uploaded_file_info = ol.upload_file('/usr/movie/92958.jpg')
 print(uploaded_file_info)
